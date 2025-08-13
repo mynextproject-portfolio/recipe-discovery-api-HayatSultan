aa5197b9fba63923ad15f2a0dc222f4e50b0b5fe
Hayat Sultan <hayatsultanm@gmail.com>
2025-08-13 19:11:34 -0400

    fix: remove duplicate app definitions

--- app/main.py
+++ app/main.py
@@ -36,14 +36,3 @@ async def get_recipe(recipe_id: int):
             return recipe
     raise HTTPException(status_code=404, detail="Recipe not found")
 
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
-
-
-app = FastAPI(openapi_url=None, docs_url=None, redoc_url=None)
-
-
-@app.get("/ping", response_class=PlainTextResponse)
-async def ping() -> str:
-	return "pong"
-
